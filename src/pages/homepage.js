650a9ce6e1607b87670fbdf1dd467458ce249b05
Cwoodard2 <cawooda@siue.edu>
2022-04-21 17:35:51 -0500

    Building the homepage and designing a theme

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -3,20 +3,103 @@ import { HashRouter, Routes, Route, Link } from "react-router-dom";
 import Navigation from '../components/navigation';
 import StandardPage from "../components/DefaultPage";
 import '../components/navigation.css';
+import './homepage.css';
 
 export default function Homepage() {
 
     return (
         <StandardPage>
             <Navigation />
-            <div className="homeMain">
                 <div className="homepageNav">
-                    <h3>Greetings, <br></br>if you got here I guess you're looking at my portfolio!<br></br>This portfolio is meant to showcase my projects, <br></br>a little about me, and increase my aptitude in design and web development</h3>
+                    <div className="navText">
+                        <h3>Greetings, <br></br>if you got here I guess you're looking at my portfolio!<br></br>This portfolio is meant to showcase my projects, <br></br>a little about me, and increase my aptitude in design and web development</h3>
+                    </div>
+                    <h3>This is some text to push the button to the left</h3>
                     <Link to="/Resume">
                         <button className="navButtonHome">Resume</button>
                     </Link>
                 </div>
-            </div>
+                <div className="homepageNav">
+                    <div className="navText">
+                        <h3>Greetings, <br></br>if you got here I guess you're looking at my portfolio!<br></br>This portfolio is meant to showcase my projects, <br></br>a little about me, and increase my aptitude in design and web development</h3>
+                    </div>
+                    <h3>This is some text to push the button to the left</h3>
+                    <Link to="/Resume">
+                        <button className="navButtonHome">Resume</button>
+                    </Link>
+                </div>
+                <div className="homepageNav">
+                    <div className="navText">
+                        <h3>Greetings, <br></br>if you got here I guess you're looking at my portfolio!<br></br>This portfolio is meant to showcase my projects, <br></br>a little about me, and increase my aptitude in design and web development</h3>
+                    </div>
+                    <h3>This is some text to push the button to the left</h3>
+                    <Link to="/Resume">
+                        <button className="navButtonHome">Resume</button>
+                    </Link>
+                </div>
+                <div className="homepageNav">
+                    <div className="navText">
+                        <h3>Greetings, <br></br>if you got here I guess you're looking at my portfolio!<br></br>This portfolio is meant to showcase my projects, <br></br>a little about me, and increase my aptitude in design and web development</h3>
+                    </div>
+                    <h3>This is some text to push the button to the left</h3>
+                    <Link to="/Resume">
+                        <button className="navButtonHome">Resume</button>
+                    </Link>
+                </div>
+                <div className="homepageNav">
+                    <div className="navText">
+                        <h3>Greetings, <br></br>if you got here I guess you're looking at my portfolio!<br></br>This portfolio is meant to showcase my projects, <br></br>a little about me, and increase my aptitude in design and web development</h3>
+                    </div>
+                    <h3>This is some text to push the button to the left</h3>
+                    <Link to="/Resume">
+                        <button className="navButtonHome">Resume</button>
+                    </Link>
+                </div>
+                <div className="homepageNav">
+                    <div className="navText">
+                        <h3>Greetings, <br></br>if you got here I guess you're looking at my portfolio!<br></br>This portfolio is meant to showcase my projects, <br></br>a little about me, and increase my aptitude in design and web development</h3>
+                    </div>
+                    <h3>This is some text to push the button to the left</h3>
+                    <Link to="/Resume">
+                        <button className="navButtonHome">Resume</button>
+                    </Link>
+                </div>
+                <div className="homepageNav">
+                    <div className="navText">
+                        <h3>Greetings, <br></br>if you got here I guess you're looking at my portfolio!<br></br>This portfolio is meant to showcase my projects, <br></br>a little about me, and increase my aptitude in design and web development</h3>
+                    </div>
+                    <h3>This is some text to push the button to the left</h3>
+                    <Link to="/Resume">
+                        <button className="navButtonHome">Resume</button>
+                    </Link>
+                </div>
+                <div className="homepageNav">
+                    <div className="navText">
+                        <h3>Greetings, <br></br>if you got here I guess you're looking at my portfolio!<br></br>This portfolio is meant to showcase my projects, <br></br>a little about me, and increase my aptitude in design and web development</h3>
+                    </div>
+                    <h3>This is some text to push the button to the left</h3>
+                    <Link to="/Resume">
+                        <button className="navButtonHome">Resume</button>
+                    </Link>
+                </div>
+                <div className="homepageNav">
+                    <div className="navText">
+                        <h3>Greetings, <br></br>if you got here I guess you're looking at my portfolio!<br></br>This portfolio is meant to showcase my projects, <br></br>a little about me, and increase my aptitude in design and web development</h3>
+                    </div>
+                    <h3>This is some text to push the button to the left</h3>
+                    <Link to="/Resume">
+                        <button className="navButtonHome">Resume</button>
+                    </Link>
+                </div>
+                <div className="homepageNav">
+                    <div className="navText">
+                        <h3>Greetings, <br></br>if you got here I guess you're looking at my portfolio!<br></br>This portfolio is meant to showcase my projects, <br></br>a little about me, and increase my aptitude in design and web development</h3>
+                    </div>
+                    <h3>This is some text to push the button to the left</h3>
+                    <Link to="/Resume">
+                        <button className="navButtonHome">Resume</button>
+                    </Link>
+                </div>
         </StandardPage>
     );
 };
