8e0ff2fddfafbac66a9304831aebd4f2a9408706
Cwoodard2 <cawooda@siue.edu>
2022-04-24 19:07:46 -0500

    Trying ideas for project page

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navigation from '../components/navigation';
 import StandardPage from "../components/DefaultPage";
+import './Portfolio.css';
 
 
 export default function Portfolio() {
@@ -12,6 +13,22 @@ export default function Portfolio() {
             <h1>We have the URCA Project</h1>
             <h1>We have the website for 234</h1>
             <h1>We have 321 project</h1>
+            {/* <div className="projectMoreInfo">
+                This is some info for a project that I am doing. We are doing some great things here.
+            </div> */}
+            <div className="portfolioMain">
+                <h1>We have the URCA Project</h1>
+                <div className="perspectiveDiv">
+                    <div className="persepctiveItem">
+                        <p>Div Item</p>
+                    </div>
+                </div>
+                <div className="perspectiveDiv">
+                    <div className="persepctiveItem">
+                        <p>Div Item</p>
+                    </div>
+                </div>
+            </div>
         </StandardPage>
     );
 };
