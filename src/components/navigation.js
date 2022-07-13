9dc102a88d0159e1db37e63794a3f1231282279d
Cwoodard2 <cawooda@siue.edu>
2022-07-13 01:50:49 -0500

    Added logo to nav bar

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,17 +1,17 @@
 import React from "react";
 import './navigation.css';
-import '../Images/Logoforwebsite.png';
+import logo from '../Images/logoWithColorBrain.png';
 import NavButton from "./NavButton";
 
 export default function Navigation() {
     return (
         <div className="navbar">
-            {/* <img src="/Users/cameronwoodard/Cwoodard2.github.io/src/Images/Logo for website.png"></img> */}
-            <NavButton page="welcome" thisPage="Welcome" thisStyle="navButton" />
-            <NavButton page="adventures" thisPage="Adventures" thisStyle="navButton" />
-            <NavButton page="skills" thisPage="Skills" thisStyle="navButton" />
-            {/* <NavButton page="/Resume" thisPage="Resume" thisStyle="navButton" /> */}
-            {/* <NavButton page="/Playground" thisPage="Playground" thisStyle="navButton" /> */}
+                <img src={logo} className="navLogo"></img>
+                <NavButton page="welcome" thisPage="Welcome" thisStyle="navButton" />
+                <NavButton page="adventures" thisPage="Adventures" thisStyle="navButton" />
+                <NavButton page="skills" thisPage="Skills" thisStyle="navButton" />
+                {/* <NavButton page="/Resume" thisPage="Resume" thisStyle="navButton" /> */}
+                {/* <NavButton page="/Playground" thisPage="Playground" thisStyle="navButton" /> */}
         </div>
     )
 }
