05df98b496fdb25b39b1d0942128c9d831f87051
Cwoodard2 <cawooda@siue.edu>
2024-06-04 11:19:47 -0500

    Updated navigation to not be so round and removed unecessary nav items

--- src/components/NavButton.js
+++ src/components/NavButton.js
@@ -1,16 +1,15 @@
 import React from "react";
-import {Link} from "react-scroll";
-import './NavButton.css';
-import homeImage from '../Images/home-icon.png';
+import { Link } from "react-scroll";
+import "./NavButton.css";
 
 export default function NavButton(props) {
-    var pagePointer = props.page;
-    var toPage = props.thisPage;
-    var style = props.thisStyle;
-    
-    return (
-        <Link to={pagePointer} spy={true} smooth={true}>
-            <button className={style}>{toPage}</button>
-        </Link>
-    );
+  var pagePointer = props.page;
+  var toPage = props.thisPage;
+  var style = props.thisStyle;
+
+  return (
+    <Link to={pagePointer} spy={true} smooth={true}>
+      <button className={style}>{toPage}</button>
+    </Link>
+  );
 }
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,17 +1,28 @@
 import React from "react";
-import './navigation.css';
-import logo from '../Images/logoWithColorBrain.png';
+import "./navigation.css";
+import logo from "../Images/logoWithColorBrain.png";
 import NavButton from "./NavButton";
 
 export default function Navigation() {
-    return (
-        <div className="navbar">
-                <img src={logo} className="nav-logo"></img>
-                <div style={{display: "flex", flexDirection: "row", gap: "2vw"}}>
-                    <NavButton page="welcome" thisPage="Welcome" thisStyle="nav-button" />
-                    <NavButton page="skills" thisPage="Skills" thisStyle="nav-button" />
-                    <NavButton page="adventures" thisPage="Projects" thisStyle="nav-button" />
-                </div>
-        </div>
-    )
+  return (
+    <div className="navbar">
+      <img src={logo} className="nav-logo"></img>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: "2vw",
+          marginLeft: "auto",
+          paddingRight: "10px",
+        }}
+      >
+        <NavButton page="skills" thisPage="Skills" thisStyle="nav-button" />
+        <NavButton
+          page="adventures"
+          thisPage="Projects"
+          thisStyle="nav-button"
+        />
+      </div>
+    </div>
+  );
 }
